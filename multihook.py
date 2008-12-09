b43c1c0188bfc8acab70ef25cc2f5e1b4ec73d06
Jack Moffitt <jack@metajack.im>
2008-12-09 09:28:17 -0700

    First cut of multihook.py.

---
+++ multihook.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+
+HOOKS = [
+    #['hook-script'], ['hook-script', 'arg1', 'arg2']
+]
+
+if __name__ == '__main__':
+    data = sys.stdin.read()
+    
+    for hook in HOOKS:
+	p = subprocess.Popen(hook, stdin=subprocess.PIPE,
+			     stdout=subprocess.PIPE)
+	p.stdin.write(data)
+	p.stdin.close()
+
+	sys.stdout.write(p.stdout.read())
+
+	
